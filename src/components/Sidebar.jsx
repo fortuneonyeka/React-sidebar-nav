fb433a23e8713fe6883a8bb56e1dbf3a906d6581
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-12 09:10:00 +0100

    added state to the navicon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,54 +1,62 @@
-import React from 'react'
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-import * as FaIcon from "react-icons/fa"
-import * as AiIcon from "react-icons/ai"
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import * as FaIcon from "react-icons/fa";
+import * as AiIcon from "react-icons/ai";
 
 const Nav = styled.div`
-background:var(--color-bg);
-height: var(--height);
-display:flex;
-justify-content:flex-start;
-align-items:center;
-`
+  background: var(--color-bg);
+  height: var(--height);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 const NavIcon = styled(Link)`
-margin-left: 2rem;
-font-size:rem;
-height:80px;
-display:flex;
-justify-content:flex-start;
-alig-items:center;
-`
+  margin-left: 2rem;
+  font-size: rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  alig-items: center;
+`;
 
-const sidebarNav = styled.nav`
-background:var(--color-bg);
-width:250px;
-height:100vh;
+const SidebarNav = styled.nav`
+background: var(--color-bg);
+  width: 250px;
+  height: 100vh;
+  display:flex;
+  justify-content: center;
+  position:fixed;
+  top:0;
+  left: ${({sidebar}) => (sidebar ? "0" : "-100%")};
+  transition:350m;
+  z-index:10;
 `
 
 const SidebarWrap = styled.div`
-width:100%;
-`
+  width: 100%;
+`;
 
 const Sidebar = () => {
+  const [sidebar, setSideBar] = useState(false);
+  const showSidebar = () => setSideBar(!sidebar);
   return (
     <>
-  <Nav>
-    <NavIcon to="#">
-    <FaIcon.FaBars />
-    </NavIcon>
-    <sidebarNav>
-      <SidebarWrap>
-      <NavIcon to="#">
-    <AiIcon.AiOutlineClose />
-    </NavIcon>
-      </SidebarWrap>
-    </sidebarNav>
-  </Nav>
+      <Nav>
+        <NavIcon to="#">
+          <FaIcon.FaBars onClick={showSidebar}/>
+        </NavIcon>
+      </Nav>
+      <SidebarNav sidebar={sidebar}>
+        <SidebarWrap>
+          <NavIcon to="#">
+            <AiIcon.AiOutlineClose onClick={showSidebar}/>
+          </NavIcon>
+        </SidebarWrap>
+      </SidebarNav>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
