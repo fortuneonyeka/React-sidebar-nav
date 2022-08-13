1da908bb987ff40debb63daece9d6b26fe109959
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-13 09:37:56 +0100

    added sidebarlink styling including hover

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -6,9 +6,24 @@ import styled from "styled-components"
 const SidebarLink = styled(Link)`
 display: flex;
 color: #e1e9fc;
+justify-content:space-between;
+align-items: center;
+list-style:none;
+padding:20px;
+height:60px;
+text-decoration:none;
+font-size:20px;
+
+&:hover {
+  background: #252831;
+  border-left: 5px solid #00FF00;
+  border-radius: 15px;
+  cursor:pointer;
+}
 `
 
 const SidebarLabel =  styled.span`
+margin-left: 16px;
 `;
 
 const SubMenu = ({item}) => {
